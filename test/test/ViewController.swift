2e478fe1c0fd33360bbb5c4718fc971e52445538
Zhenya Loyko <e.lojko@inbox.ru>
2023-10-19 22:02:34 +0300

    Project created

---
+++ test/test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Евгений Лойко on 19.10.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
